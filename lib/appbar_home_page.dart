b46d0fafff82d0100b7fe79e9a98da8c0952a09b
Pallavi Patil <pallavishravan545@gmail.com>
2025-03-28 11:57:26 +0530

    body SliverAppBar

--- lib/appbar_home_page.dart
+++ lib/appbar_home_page.dart
@@ -21,7 +21,7 @@ class _AppbarHomePageState extends State<AppbarHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         backgroundColor: Colors.deepPurple.shade800,
         foregroundColor: Colors.white,
         leading: IconButton(onPressed: (){},
@@ -95,6 +95,94 @@ class _AppbarHomePageState extends State<AppbarHomePage> {
            }),
          ],
 
+      ),*/
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+
+            floating: true,
+              pinned: false,
+              snap: true,
+              expandedHeight: 200,
+
+              backgroundColor: Colors.deepPurple.shade800,
+              foregroundColor: Colors.white,
+              leading: IconButton(onPressed: (){},
+                  icon: Icon(Icons.account_circle,size: 27,)
+              ),
+              leadingWidth: 70,
+              title: Text("Home"),
+              centerTitle: true,
+              //round border
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(bottom: Radius.circular(5)),
+                //border
+                side: BorderSide(width: 6,color: Colors.lightGreen.withOpacity(0.5),strokeAlign: BorderSide.strokeAlignOutside),
+              ),
+              elevation: 20,
+              shadowColor: Colors.blueGrey,
+
+              //for big size
+              bottom: PreferredSize(
+                  preferredSize: Size(double.infinity,300),
+                  child: Container()
+              ),
+              flexibleSpace: Container(height: 200,color: Colors.green,),
+              actions: [
+                IconButton(onPressed: (){}, icon:Icon(Icons.camera_alt_outlined)),
+                IconButton(onPressed: (){}, icon:Icon(Icons.settings)),
+                PopupMenuButton(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                    shadowColor: Colors.black,
+                    color: Colors.orange.shade300,
+                    initialValue: mInitialValue,
+                    onSelected: (value){
+                      mInitialValue=value.toString();
+                      setState(() {
+
+                      });
+                    },
+                    itemBuilder: (_){
+                      return [
+                        PopupMenuItem(
+                            value:"Settings" ,
+                            onTap: (){
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.settings),
+                                SizedBox(width: 10,),
+                                Text("Settings"),
+                              ],
+                            )),
+                        PopupMenuItem(
+                            value:"info" ,
+                            onTap: (){},
+                            child: Row(
+                              children: [
+                                Icon(Icons.info),
+                                SizedBox(width: 10,),
+                                Text("info"),
+                              ],
+                            )),
+                        PopupMenuItem(
+                            value:"account" ,
+                            onTap: (){},
+                            child: Row(
+                              children: [
+                                Icon(Icons.account_circle),
+                                SizedBox(width: 10,),
+                                Text("account"),
+                              ],
+                            )),
+                      ];
+                    }),
+              ],
+
+          ),
+          SliverList(delegate: SliverChildBuilderDelegate((ctx,index){
+            return ListTile(title: Text("Hello"),);
+          }))
+        ],
       ),
 
     );
